22dcbb33e29f3612eefa9ee1137ddbe8529ed7e9
xjq <lebnizjoan@gmail.com>
2021-12-12 22:07:04 +0800

    feat: safeArea

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
-import * as React from 'react';
+import React from 'react';
 import Toast from 'react-native-toast-message';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Stack from './navigation/Stack';
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <NavigationContainer>
         <Stack />
       </NavigationContainer>
       <Toast position="top" bottomOffset={20} />
-    </>
+    </SafeAreaProvider>
   );
 }
